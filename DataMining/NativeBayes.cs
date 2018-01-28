36d5cc16e69cbd26faaea8f65a7cc7fb3d9ff164
赵安清 <zaqcloud@hotmail.com>
2018-01-28 21:18:43 +0800

    Add the implementation of Native Bayes classifier.

---
+++ DataMining/NativeBayes.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pure.DataMining
+{
+    public sealed class NativeBayesResult<TTarget>
+    {
+        public TTarget Target
+        {
+            get;
+        }
+
+        public double Probability
+        {
+            get;
+        }
+
+        internal NativeBayesResult(TTarget target, double probability)
+        {
+            this.Target = target;
+            this.Probability = probability;
+        }
+    }
+
+    public class NativeBayes<TProperty, TTarget>
+    {
+        private List<TTarget> targetItems;
+        private Dictionary<string, NativeBayesProperty<TProperty, TTarget>> propertyItems;
+
+        internal int TargetCount
+        {
+            get { return this.targetItems.Count; }
+        }
+
+        public NativeBayes(params TTarget[] targets)
+        {
+            this.targetItems = targets.ToList();
+            this.propertyItems = new Dictionary<string, NativeBayesProperty<TProperty, TTarget>>();
+        }
+
+        public NativeBayesProperty<TProperty, TTarget> AddProperty(string name)
+        {
+            var property = new NativeBayesProperty<TProperty, TTarget>(this);
+            propertyItems.Add(name, property);
+            return property;
+        }
+
+        public NativeBayesResult<TTarget> Perform(IDictionary<string, TProperty> input)
+        {
+            if (input.Count > propertyItems.Count)
+            {
+                throw new ArgumentException();
+            }
+
+            TTarget target = default(TTarget);
+            double probability = 0.0;
+
+            for (int targetIndex = 0; targetIndex < targetItems.Count; targetIndex++)
+            {
+                double targetProbability = 1.0;
+
+                foreach (var pair in input)
+                {
+                    NativeBayesProperty<TProperty, TTarget> propertyItem;
+                    bool result = propertyItems.TryGetValue(pair.Key, out propertyItem);
+
+                    if (result)
+                    {
+                        double percentage = propertyItem.GetPropertyPercentage(pair.Value, targetIndex);
+                        targetProbability *= percentage;
+                    }
+                    else
+                    {
+                        throw new ArgumentException();
+                    }
+                }
+
+                double aa = propertyItems.Sum(o => o.Value.GetTotalCount(targetIndex)) / propertyItems.Sum(o => o.Value.GetTotalCount());
+                targetProbability *= aa;
+
+                if (probability < targetProbability)
+                {
+                    probability = targetProbability;
+                    target = targetItems[targetIndex];
+                }
+            }
+
+            return new NativeBayesResult<TTarget>(target, probability);
+        }
+    }
+
+    public class NativeBayesProperty<TProperty, TTarget>
+    {
+        private NativeBayes<TProperty, TTarget> parent;
+        private Dictionary<TProperty, IList<int>> propertyItems;
+        private IList<int> totalCounters;
+
+        public ReadOnlyDictionary<TProperty, IList<int>> PropertyItems
+        {
+            get { return new ReadOnlyDictionary<TProperty, IList<int>>(propertyItems); }
+        }
+
+        internal NativeBayesProperty(NativeBayes<TProperty, TTarget> parent)
+        {
+            this.propertyItems = new Dictionary<TProperty, IList<int>>();
+            this.totalCounters = Enumerable.Repeat(0, parent.TargetCount).ToList();
+            this.parent = parent;
+        }
+
+        public void AddPropertyItem(TProperty item, params int[] counters)
+        {
+            if (this.parent.TargetCount != counters.Length)
+            {
+                throw new ArgumentException();
+            }
+
+            this.propertyItems.Add(item, counters);
+
+            for (int i = 0; i < this.parent.TargetCount; i++)
+            {
+                this.totalCounters[i] += counters[i];
+            }
+        }
+
+        public double GetPropertyPercentage(TProperty property, int targetIndex)
+        {
+            return PropertyItems[property][targetIndex] * 1.0 / totalCounters[targetIndex];
+        }
+
+        public double GetTotalCount(int targetIndex)
+        {
+            return totalCounters[targetIndex];
+        }
+
+        public double GetTotalCount()
+        {
+            return totalCounters.Sum();
+        }
+    }
+}
---
+++ DataMiningUnitTests/NativeBayesUnitTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pure.DataMining;
+
+namespace Pure.DataMining.UnitTests
+{
+    [TestClass]
+    public class NativeBayesUnitTests
+    {
+        [TestMethod]
+        public void TestNativeBayes()
+        {
+            bool[] buyComputer = { true, false };
+
+            int[][] ageArray = new int[][]
+            {
+                new int[] { 2, 3 }, // youth
+                new int[] { 4, 0 }, // middle-aged
+                new int[] { 3, 2 }  // senior
+            };
+
+            int[][] incomeArray = new int[][]
+            {
+                new int[] { 3, 1 }, // low
+                new int[] { 4, 2 }, // medium
+                new int[] { 2, 2 }  // high
+            };
+
+            int[][] studentArray = new int[][]
+            {
+                new int[] { 6, 1 }, // yes
+                new int[] { 3, 4 }  // no
+            };
+
+            int[][] creditArray = new int[][]
+            {
+                new int[] { 6, 2 }, // fair
+                new int[] { 3, 3 }  // excellent
+            };
+
+            string[] names = { "age", "income", "student", "credit" };
+            string[] ageItems = { "youth", "middle-aged", "senior" };
+            string[] incomeItems = { "low", "medium", "high" };
+            string[] studentItems = { "yes", "no" };
+            string[] creditItems = { "fair", "excellent" };
+
+            var bayes = new NativeBayes<string, bool>(buyComputer);
+
+            var age = bayes.AddProperty(names[0]);
+            age.AddPropertyItem(ageItems[0], ageArray[0]);
+            age.AddPropertyItem(ageItems[1], ageArray[1]);
+            age.AddPropertyItem(ageItems[2], ageArray[2]);
+
+            var income = bayes.AddProperty(names[1]);
+            income.AddPropertyItem(incomeItems[0], incomeArray[0]);
+            income.AddPropertyItem(incomeItems[1], incomeArray[1]);
+            income.AddPropertyItem(incomeItems[2], incomeArray[2]);
+
+            var student = bayes.AddProperty(names[2]);
+            student.AddPropertyItem(studentItems[0], studentArray[0]);
+            student.AddPropertyItem(studentItems[1], studentArray[1]);
+
+            var credit = bayes.AddProperty(names[3]);
+            credit.AddPropertyItem(creditItems[0], creditArray[0]);
+            credit.AddPropertyItem(creditItems[1], creditArray[1]);
+
+            Dictionary<string, string> testProperties = new Dictionary<string, string>()
+            {
+                { names[0], ageItems[0] },
+                { names[1], incomeItems[1] },
+                { names[2], studentItems[0] },
+                { names[3], creditItems[0] }
+            };
+
+            var result = bayes.Perform(testProperties);
+            Assert.AreEqual(true, result.Target);
+            Assert.AreEqual(0.028, result.Probability, 0.001);
+        }
+    }
+}
